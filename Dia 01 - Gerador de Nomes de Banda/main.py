62cbcdf64b3822a772c45e7b0bb6de6fbf0ab32b
Wellington Holanda <102003595+wellingtonh07@users.noreply.github.com>
2024-07-26 19:02:06 -0300

    Dia 01

---
+++ Dia 01 - Gerador de Nomes de Banda/main.py
@@ -0,0 +1,4 @@
+print("Bem vindo ao Gerador de Nomes de Banda!")
+cidade = input("Qual é o nome da cidade que você nasceu?\n>>> ")
+pet = input("Qual é o nome do seu animal de estimação?\n>>> ")
+print(f"O nome de sua banda vai ser {cidade} {pet}")
