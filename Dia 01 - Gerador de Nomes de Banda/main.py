0053db7f72433fcd2d0c58e45fdda5cfd40e01bb
Wellington Holanda <102003595+wellingtonh07@users.noreply.github.com>
2025-01-15 19:48:30 -0300

    Update main.py

--- Dia 01 - Gerador de Nomes de Banda/main.py
+++ Dia 01 - Gerador de Nomes de Banda/main.py
@@ -1,4 +1,11 @@
+# Exibe o título do programa
 print("Bem vindo ao Gerador de Nomes de Banda!")
+
+# Solicita o nome da cidade onde o usuário nasceu
 cidade = input("Qual é o nome da cidade que você nasceu?\n>>> ")
+
+# Solicita o nome do animal de estimação do usuário
 pet = input("Qual é o nome do seu animal de estimação?\n>>> ")
+
+# Exibe o nome da banda gerado, combinando a cidade com o nome do pet
 print(f"O nome de sua banda vai ser {cidade} {pet}")
