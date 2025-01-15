850584d06efacb5f3d0e3429c40146149fe6d055
Wellington Holanda <102003595+wellingtonh07@users.noreply.github.com>
2025-01-15 19:53:23 -0300

    Update main.py

--- Dia 04 - Pedra, Papel e Tesoura/main.py
+++ Dia 04 - Pedra, Papel e Tesoura/main.py
@@ -1,5 +1,7 @@
+# Importa a biblioteca random para gerar a escolha do computador de forma aleatória
 import random
 
+# Representações visuais de Pedra, Papel e Tesoura
 pedra = '''
     _______
 ---'   ____)
@@ -27,19 +29,25 @@ tesoura = '''
 ---.__(___)
 '''
 
+# Lista que contém as representações das opções de Pedra, Papel e Tesoura
 imagem_do_jogo = [pedra, papel, tesoura]
+
+# Solicita ao usuário para escolher entre Pedra (0), Papel (1) ou Tesoura (2)
 escolha_usuario = int(input("Qual você escolhe? Digite 0 para Pedra, 1 para Papel e 2 para Tesoura \n>> "))
-if escolha_usuario >= 3 or escolha_usuario < 0 :
+
+# Verifica se a escolha do usuário é válida (0, 1 ou 2)
+if escolha_usuario >= 3 or escolha_usuario < 0:
     print("Você digitou um número inválido. Você perdeu!")
 else:
+    # Exibe a escolha do usuário
     print(imagem_do_jogo[escolha_usuario])
 
-
+    # O computador faz uma escolha aleatória entre Pedra, Papel e Tesoura
     escolha_computador = random.randint(0, 2)
     print("Computador escolhe:")
     print(imagem_do_jogo[escolha_computador])
 
-
+    # Verifica as condições de vitória, derrota ou empate
     if escolha_usuario == 0 and escolha_computador == 2:
         print("Você VENCEU!")
     elif escolha_computador == 0 and escolha_usuario == 2:
